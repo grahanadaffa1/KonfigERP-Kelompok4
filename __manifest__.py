0f3884fbac95a1d460ff0ad0eb970f95f2f4d08d
unknown <grahanadaffa1@gmail.com>
2021-06-11 21:11:39 +0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+        'name':'Name Card 123',
+        'description':'Create name card 123',
+        'summary':'Create name card 123',
+        'author':'Kelompok 4',
+        'depends':['base','contacts'],
+        'application':False,
+        'version':'1.0',
+        'license':'AGPL-3',
+        'support': 'yvandotet@yahoo.fr',
+        'website':'https://github.com/YvanDotet/print_contact',
+        
+        'data':[
+            'views/print_buttons.xml',
+            
+            'report/print_page.xml',
+            ],
+         
+        'images': ['images/thumbnail.png'],
+}
+
